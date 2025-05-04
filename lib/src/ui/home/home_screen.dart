04c9cb28edbfb43cd9613c59dabd533c73207201
isaacscardoso <isaacscardoso@protonmail.com>
2025-05-04 03:05:23 -0300

    feat(I-002): Transforms the HomeScreen class into a stateful one. Defines the countdownController variable and uses it as a parameter in the CountdownWidget instance. Creates the enableResendSMS variable and manipulates it through the ValueNotifier. Redraws the CustomElevatedButton through a ValueListenableBuilder.

--- lib/src/ui/home/home_screen.dart
+++ lib/src/ui/home/home_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
-import '../core/countdown/countdown_widget.dart';
+import '../core/countdown/countdown.dart';
 import '../widgets/custom_elevated_button.dart';
 
-final class HomeScreen extends StatelessWidget {
+final class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+final class _HomeScreenState extends State<HomeScreen> {
+  late final CountdownController countdownController;
+  ValueNotifier<bool> enableResendSMS = ValueNotifier(false);
+
+  @override
+  void initState() {
+    super.initState();
+    countdownController = CountdownController(
+      duration: const Duration(seconds: 10),
+      onEnd: () {
+        enableResendSMS.value = true;
+      },
+    );
+  }
+
+  void _onResendSMSPressed() {
+    enableResendSMS.value = false;
+    countdownController.restart();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +39,22 @@ final class HomeScreen extends StatelessWidget {
           spacing: 10,
           children: <Widget>[
             CountdownWidget(
-              duration: const Duration(minutes: 1),
-              textStyle: const TextStyle(fontSize: 30),
+              controller: countdownController,
+              textStyle: TextStyle(fontSize: 30),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               spacing: 10,
               children: <Widget>[
-                CustomElevatedButton(text: 'Reset', onPressed: () {}),
-                CustomElevatedButton(text: 'Resend SMS', onPressed: () {}),
+                ValueListenableBuilder(
+                  valueListenable: enableResendSMS,
+                  builder: (_, value, _) {
+                    return CustomElevatedButton(
+                      text: 'Resend SMS',
+                      onPressed: value ? _onResendSMSPressed : null,
+                    );
+                  },
+                ),
               ],
             ),
           ],
