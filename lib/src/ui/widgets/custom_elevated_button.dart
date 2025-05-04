5cd000bf999c8a1a376102cc8f4ae1a9350bc8bf
isaacscardoso <isaacscardoso@protonmail.com>
2025-05-04 03:00:12 -0300

    feat(I-002): Makes the onPressed attribute nullable.

--- lib/src/ui/widgets/custom_elevated_button.dart
+++ lib/src/ui/widgets/custom_elevated_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 final class CustomElevatedButton extends StatelessWidget {
   final String text;
-  final VoidCallback onPressed;
+  final VoidCallback? onPressed;
   final BorderRadiusGeometry borderRadius;
   final TextStyle textStyle;
   final EdgeInsetsGeometry? padding;
